3a7784400fbadf0acbf16dcf971ff274d9bed03a
srkicp3 <101930713+srkicp3@users.noreply.github.com>
2022-04-30 12:59:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+	function myFunction() {
+	var sastojci1 = document.getElementById("myPopup");
+	sastojci1.classList.toggle("show");
+}
+	function myFunction2() {
+	var sastojci1 = document.getElementById("myPopup2");
+	sastojci1.classList.toggle("show");
+}
+	function myFunction3() {
+	var sastojci1 = document.getElementById("myPopup3");
+	sastojci1.classList.toggle("show");
+
+}
+	function myFunction4() {
+	var sastojci1 = document.getElementById("myPopup4");
+	sastojci1.classList.toggle("show");
+
+}
+	function myFunction5() {
+	var sastojci1 = document.getElementById("myPopup5");
+	sastojci1.classList.toggle("show");
+
+}
+	function myFunction6() {
+	var sastojci1 = document.getElementById("myPopup6");
+	sastojci1.classList.toggle("show");
+}
+	/*
+	var x=4;
+	function IspisiKorisnikaKonzola(){
+		console.log("Test: x = " + x);
+		console.log("Korisnik: "+korisnik.ime + " " +korisnik.prezime);
+	}
+	function IspisiKorisnikaNaSajtu(){
+		document.getElementById("korisnik").innerHTML = korisnik.printInfo();;
+		
+	}
+	var korisnik={
+		ime: "Srđan",
+		prezime: "Nikolić",
+		printInfo: function(){return this["ime"] + " " + this["prezime"];}
+	}
+	*/
+
+	
