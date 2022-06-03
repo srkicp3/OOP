b9abff18a22a1113c83a67777c197074345ed344
srkicp3 <101930713+srkicp3@users.noreply.github.com>
2022-06-03 23:24:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 	var sastojci1 = document.getElementById("myPopup");
 	sastojci1.classList.toggle("show");
 }
+
 	function myFunction2() {
 	var sastojci1 = document.getElementById("myPopup2");
 	sastojci1.classList.toggle("show");
@@ -26,6 +27,22 @@
 	var sastojci1 = document.getElementById("myPopup6");
 	sastojci1.classList.toggle("show");
 }
+
+	function myFunction7() {
+	var sastojci1 = document.getElementById("myPopup7");
+	sastojci1.classList.toggle("show");
+}
+
+	function myFunction8() {
+	var sastojci1 = document.getElementById("myPopup8");
+	sastojci1.classList.toggle("show");
+}
+
+	function myFunction9() {
+	var sastojci1 = document.getElementById("myPopup9");
+	sastojci1.classList.toggle("show");
+}
+
 	function PosaljiMejl()
 	{
 		var adr = document.getElementById('adresa').value;
@@ -98,7 +115,7 @@
           event.target.style.display = "none";
         }
       }; */
-	  var DnevniSpecijal;
+	/*   var DnevniSpecijal;
 	  fetch('./data.json')
 	  .then((response) => {
 		return response.json()
@@ -110,69 +127,203 @@
 	  })
 	  .catch((err) => {
 		// Do something for an error here
-	  })
+	  }) */
+	  
+	var DnevniSpecijal = 
+	
+	[
+		
+		{
+			Ime: "Kafa",
+			Slika: "KafaPic.jpg"
+		},
+		
+		{
+			Ime: "Smuti",
+			Slika: "SmutiPic.png"
+		},
+		
+		{
+			Ime: "Pica",
+			Slika: "PicaPic.jpg"
+			
+		}
+	];
+
+	  
+	  
+	  var meninaslov=document.createElement("div");
+	  meninaslov.className="meninaslov";
+	  var naslov= document.createElement("h3");
+	  naslov.className="h3";
+	  naslov.innerText = "Dnevni specijal";
+	  meninaslov.appendChild(naslov);
+	  document.getElementById("DnSpecijal").appendChild(meninaslov);
 	  
 	  
 	  
-	  function KreirajMeni(){
+	  
 		  
 	  var meniDnevni = document.createElement("div");
-	  meniDnevni.class="menideo1";
+	  meniDnevni.className="menideo1";
 	  document.getElementById("DnSpecijal").appendChild(meniDnevni); 
-	  document.body.insertBefore(meniDnevni, getElementById("Rezervacija") );
+	  document.body.insertBefore(meniDnevni, document.getElementById("Rezervacija") );
 	  
 	  var tabela1 = document.createElement("div");
-	  tabela1.class="tabela1";
+	  tabela1.className="tabela1";
 	  meniDnevni.appendChild(tabela1);
 	  
 	  var kolona1 = document.createElement("div");
-	  kolona1.class="kolona";
+	  kolona1.className="kolona";
 	  tabela1.appendChild(kolona1);
 	  
 	  var slika1 = document.createElement("div");
-	  slika1.class="slika1";
+	  slika1.className="slika1";
 	  kolona1.appendChild(slika1);
 	  
 	  var dugme=document.createElement("div");
-	  dugme.class="dugme";
+	  dugme.className="dugme";
 	  slika1.appendChild(dugme);
 	  
 	  var sastojci1 = document.createElement("button");
-	  sastojci1.class="sastojci1";
+	  sastojci1.className="sastojci1";
 	  sastojci1.innerText="vidi sastojke";
-	  sastojci1.onclick="myFunction4()";
+	  sastojci1.onclick = function() {myFunction7()};
 	  
 	  var sastojci1text= document.createElement("span");
-	  sastojci1text.class="sastojci1text";
-	  sastojci1text.innerText="banana, sladoled, šlag, čoko preliv";
-	  sastojci1text.id = "myPopup4";
+	  sastojci1text.className="sastojci1text";
+	  sastojci1text.innerText="kafa, mleko, šećer";
+	  sastojci1text.id = "myPopup7";
 	  dugme.appendChild(sastojci1);
 	  sastojci1.appendChild(sastojci1text);
 	  
 	  var dugme2 = document.createElement("div");
-	  dugme2.class=dugme;
+	  dugme2.className="dugme";
 	  
 	  var sastojci2 = document.createElement("button");
-	  sastojci2.class="sastojci2";
+	  sastojci2.className="sastojci2";
 	  sastojci2.innerText="poruči";
-	  /* sastojci2.data-modal= "modalOne1"; */
-	  sastojci2.class="modal12";
+	  /* var modal=document.getElementById("modalOne1");
+	  sastojci2.data= modal.id;   */
+	  /* sastojci2.data-modal="modal2"; */ /* postoji data-modal ali mi daje error u konzoli kada ga iskoristim*/
+	  sastojci2.data="modal2";
 	  dugme2.appendChild(sastojci2);
 	  slika1.appendChild(dugme2);
 	  
-	  var slikaimg = '<img src="'+ DnevniSpecijal[0].Slika+'" />';
+	  
 	  var slikaKafa=document.createElement("img");
-	  slikaKafa.innerHTML = slikaimg;
-	  slikaKafa.height = "350px";
-	  slikaKafa.width = "450px";
-	  slikaKafa.border = "1 px";
+	  slikaKafa.src= DnevniSpecijal[0].Slika;
+	  slikaKafa.className="slikaKafa";
 	  slika1.appendChild(slikaKafa);
 	  
 	  var h31 = document.createElement("h3");
-	  h31.class="h31";
-	  h31.innerText="Kafa";
+	  h31.className="h31";
+	  h31.innerText=DnevniSpecijal[0].Ime;
 	  kolona1.appendChild(h31);
-	  }
+	  
+	  
+	  
+	  
+	  var kolona2 = document.createElement("div");
+	  kolona2.className="kolona";
+	  tabela1.appendChild(kolona2);
+	  
+	  var slika2 = document.createElement("div");
+	  slika2.className="slika1";
+	  kolona2.appendChild(slika2);
+	  
+	  var dugme1=document.createElement("div");
+	  dugme1.className="dugme";
+	  slika2.appendChild(dugme1);
+	  
+	  var sastojci2 = document.createElement("button");
+	  sastojci2.className="sastojci1";
+	  sastojci2.innerText="vidi sastojke";
+	  sastojci2.onclick = function() {myFunction8()};
+	  
+	  var sastojci2text= document.createElement("span");
+	  sastojci2text.className="sastojci1text";
+	  sastojci2text.innerText="jagoda, borovnica, kivi";
+	  sastojci2text.id = "myPopup8";
+	  dugme1.appendChild(sastojci2);
+	  sastojci2.appendChild(sastojci2text);
+	  
+	  var dugme22 = document.createElement("div");
+	  dugme22.className="dugme";
+	  
+	  var sastojci22 = document.createElement("button");
+	  sastojci22.className="sastojci2";
+	  sastojci22.innerText="poruči";
+	  var modal=document.getElementById("modalOne1");		/*još neki pokušaji*/
+	  sastojci2.data= modal.id;  
+	  /* sastojci2.className="modal2"; */
+	  dugme22.appendChild(sastojci22);
+	  slika2.appendChild(dugme22);
+	  
+	  
+	  var slikaSmuti=document.createElement("img");
+	  slikaSmuti.src= DnevniSpecijal[1].Slika;
+	  slikaSmuti.className="slikaKafa";
+	  slika2.appendChild(slikaSmuti);
+	  
+	  var h312 = document.createElement("h3");
+	  h312.className="h31";
+	  h312.innerText=DnevniSpecijal[1].Ime;
+	  kolona2.appendChild(h312);
+	  
+	  
+	  
+	  
+
+	  
+	  
+	  var kolona3 = document.createElement("div");
+	  kolona3.className="kolona";
+	  tabela1.appendChild(kolona3);
+	  
+	  var slika3 = document.createElement("div");
+	  slika3.className="slika1";
+	  kolona3.appendChild(slika3);
+	  
+	  var dugme3=document.createElement("div");
+	  dugme3.className="dugme";
+	  slika3.appendChild(dugme3);
+	  
+	  var sastojci3 = document.createElement("button");
+	  sastojci3.className="sastojci1";
+	  sastojci3.innerText="vidi sastojke";
+	  sastojci3.onclick = function() {myFunction9()};
+	  
+	  var sastojci3text= document.createElement("span");
+	  sastojci3text.className="sastojci1text";
+	  sastojci3text.innerText="jaja, šunka, masline, šampinjoni";
+	  sastojci3text.id = "myPopup9";
+	  dugme3.appendChild(sastojci3);
+	  sastojci3.appendChild(sastojci3text);
+	  
+	  var dugme32 = document.createElement("div");
+	  dugme32.className="dugme";
+	  
+	  var sastojci32 = document.createElement("button");
+	  sastojci32.className="sastojci2";
+	  sastojci32.innerText="poruči";
+	  var modal=document.getElementById("modalOne1");
+	  sastojci32.data= modal.id;  
+	  /* sastojci2.className="modal2"; */
+	  dugme32.appendChild(sastojci32);
+	  slika3.appendChild(dugme32);
+	  
+	  
+	  var slikaPica=document.createElement("img");
+	  slikaPica.src= DnevniSpecijal[2].Slika;
+	  slikaPica.className="slikaKafa";
+	  slika3.appendChild(slikaPica);
+	  
+	  var h313 = document.createElement("h3");
+	  h313.className="h31";
+	  h313.innerText=DnevniSpecijal[2].Ime;
+	  kolona3.appendChild(h313);
+	  
 	  
 	/* var test=document.createElement("div");
 	  test.style.backgroundColor = "blue";
